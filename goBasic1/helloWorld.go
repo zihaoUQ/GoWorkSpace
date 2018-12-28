d7d3e2e00d740767ad234d52489fee435bccd2f0
caizihao <caizihao@Claires-MacBook-Pro-2.local>
2018-12-28 11:14:51 +0800

    u know that....hello world

--- goBasic1/helloWorld.go
+++ goBasic1/helloWorld.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World");
+	foo();
+
+	for i:=0;i<100;i++ {
+		if i % 2 == 0 {
+			fmt.Println(i);
+		}
+	}
 }
+
+func foo() {
+	fmt.Println("eh")
+}
+
+
+
+/*
+Control flow:
+	1. sequential
+	2. loop or iterative
+	3. conditional
+ */
